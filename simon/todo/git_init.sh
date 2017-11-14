e9f84733a358c077020ec343ebf627a104c22428
simon <413675377@qq.com>
2017-11-14 10:44:45 +0800

    Easywork: add git_init.md manual
    TEST=None
    BUG=None

--- simon/todo/git_init.sh
+++ simon/todo/git_init.sh
@@ -2,3 +2,15 @@
 #
 # need a document to start git 
 
+# config your email
+git config --global user.name "<name>"
+git config --global user.email "<email>"
+
+# create your first repo
+mkdir <my_local_repo>
+cd <my_local_repo>
+git init
+touch README.md
+git add README.md
+git commit -m "first commit"
+git remote add origin http://gitee.com/simonhzhou123/easywork.git
